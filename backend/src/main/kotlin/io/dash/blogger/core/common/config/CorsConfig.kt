38510887c2694e412cac72cc9d468bf78e3b51db
Kangmin <ehfehf0629@gmail.com>
2024-06-03 10:17:24 +0900

    Init: CorsConfig

---
+++ backend/src/main/kotlin/io/dash/blogger/core/common/config/CorsConfig.kt
@@ -0,0 +1,16 @@
+package io.dash.blogger.core.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("http://localhost:5501")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD")
+            .allowedHeaders("*")
+    }
+}
